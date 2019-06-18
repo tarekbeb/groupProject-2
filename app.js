d4c6cf981b60193aa5568e4d24dce7e0302fdaf1
Michael Dao <michaeltuandao@gmail.com>
2019-06-18 16:03:26 -0500

    update

--- app.js
+++ app.js
@@ -1,27 +1,3 @@
-const express = require('express');
-const app = express();
-
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-
-app.use(express.static('public'));
-
-
-
-
-
-
-
-
-
-
-const port = 3000;
-app.listen(port, ()=>{
-    console.log(`Listening on port: ${port}`)
-})
-
-
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 var express = require('express');
 var session = require('express-session');
 // var bodyParser = require('body-parser');
