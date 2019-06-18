35bc807f72e34ebd49104684388d7bba936c7564
Tarek Al Beb <tarekalbeb@Tareks-MacBook-Air.local>
2019-06-18 09:57:20 -0500

    freeztable: true

--- migrations/20190618142231-create-profile.js
+++ migrations/20190618142231-create-profile.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('profiles', {
+    return queryInterface.createTable('profile', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -43,6 +43,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('profiles');
+    return queryInterface.dropTable('profile');
   }
 };
