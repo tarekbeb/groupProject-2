747b8b7756598a19bd2e8e32c469da92a4378fe7
Chris-Humphrey <clhumphrey21@gmail.com>
2019-06-19 14:00:18 -0500

    Front page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 router.get('/', ((req, res) => {
     res.render('index');
-}))
+}));
+
+router.get('/home',((req, res) => {
+    res.render('home');
+}));
 
 
 module.exports = router;
