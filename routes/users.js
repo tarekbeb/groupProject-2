bc74f8d313ed671fe30500e7701e6f73f7936e32
Michael Dao <michaeltuandao@gmail.com>
2019-06-19 00:34:37 -0500

    updated users.js

--- routes/users.js
+++ routes/users.js
@@ -11,21 +11,59 @@ router.get('/register', ((req, res) => {
     res.render('register');
 }))
 
+//REGISTER HANDLE
 router.post('/register', ((req, res) => {
-    let username = req.body.username;
-    let password = bcrip.hasSync(req.body.password, 8);
-    let email = req.body.email;
-    let name = req.body.name;
+    // const {fname, lname, email, username, password, password2} = req.body;
+    var fname = req.body.fname;
+    var lname = req.body.lname;
+    var email = req.body.email;
+    var username = req.body.username;
+    var password = req.body.password;
+    var password2 = req.body.password2;
+    let errors = [];
+
+    //CHECK REQUIRED FIELDS
+    if(!fname || !lname || !email || !username || !password || !password2){
+        errors.push({msg: 'Please fill in all fields'})
+    }
+
+    //CHECK PASSWORDS MATCH
+    if(password != password2) {
+        errors.push({msg: 'Passwords do not match'})
+    }
+
+    //CHECK PASS LENGTH
+    if(password.length < 8){
+        errors.push({msg: 'Password should at least be 6 characters long'})
+    }
+
+    if(errors.length > 0){
+        res.render('register', {
+            errors,
+            fname,
+            lname,
+            username,
+            email,
+            password,
+            password2
+        });
+    } else {
+        res.send('Pass');
+    }
+
+
+    // let password = bcrip.hasSync(req.body.password, 8);
+
     
-    db.profile.create({username: username, password:password, email:email, name:name})
-    .then((result) => {
-        // res.redirect('/login')
-        console.log(`${username}, ${name}, ${email}`);
-    })
-    .catch((error) => {
-        res.send(error)
-    })
-    res.send('register post afterwards')    
+    // db.profile.create({username: username, password:password, email:email, name:name})
+    // .then((result) => {
+    //     // res.redirect('/login')
+    //     console.log(`${username}, ${name}, ${email}`);
+    // })
+    // .catch((error) => {
+    //     res.send(error)
+    // })
+    // res.send('register post afterwards')    
 }))
 
 
