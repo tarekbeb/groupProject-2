5af53eb73ec831a5b1fe399722673c33023c8802
Michael Dao <michaeltuandao@gmail.com>
2019-06-20 07:31:14 -0500

    added dashboard friendliness

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var { ensureAuthenticated } = require('../config/auth')
 
 router.get('/', ((req, res) => {
     res.render('home');
+}))
+
+router.get('/dashboard', ensureAuthenticated, ((req, res) => {
+    res.render('dashboard');
+    //MAYBE SAY HI TO USER? PASS NAME TO EJS DASHBOARD
+    // name: req.user.fname
 }))
 
 
