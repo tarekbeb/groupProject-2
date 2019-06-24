3066e8d7ab6054a2fc1f33cbc68bf7869bf3e3e1
Alfred Santos <alfie.g.santos@gmail.com>
2019-06-23 20:40:25 -0500

    ideaPage

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,5 @@ router.get('/', ((req, res) => {
     res.render('index');
 }))
 
-router.get('/ideas', ensureAuthenticated, ((req, res) => {
-    res.render('ideas');
-    //MAYBE SAY HI TO USER? PASS NAME TO EJS DASHBOARD
-    // name: req.user.fname
-}));
-
 
 module.exports = router;
